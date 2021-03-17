8b86e67699d905d436f1abfd930f86d3076d4173
Exxon <tomfagan03@gmail.com>
2021-03-17 16:59:10 +0000

    Finished of some stuff

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,7 @@
 const reset: string = '\x1b[0m'
-const colour = {
 
-    
 
-    /**
+/**
      * @param {string} text - The text which you want to put a colour
      * @param {string} textColour - The text colour you want (Object)
      * @param {string} backgroundColour - The background colour you want (Object)
@@ -12,55 +10,56 @@ const colour = {
      * 
      * log('Hello World', {textColour: 'blue', backgroundColour: 'white'})
      */
-    log(text: string, {textColour = 'none', backgroundColour = 'none'}): string | void {
+
+const colour = (text: string, {textColour = 'none', backgroundColour = 'none'}): string | void => {
         if (!text) { return console.error("\x1b[36mCDColours \x1b[34m>> \x1b[37mNo text was provided") }
         if (typeof text != 'string') { return console.error("\x1b[36mCDColours \x1b[34m>> \x1b[37mText is not a string, received " + typeof text) }
-        if (textColour == 'none' && backgroundColour == 'none') { return console.error("\x1b[36mCDColours \x1b[34m>> \x1b[37mNo colours were specified, try \x1b[1mcolour.log('Hello World!', { textColour: 'blue', backgroundColour: 'white' })\x1b[0m instead") } // Oooo ok ill google n see
+        if (textColour == 'none' && backgroundColour == 'none') { return console.error("\x1b[36mCDColours \x1b[34m>> \x1b[37mNo colours were specified, try \x1b[1mcolour.log('Hello World!', { textColour: 'blue', backgroundColour: 'green' })\x1b[0m instead") } // Oooo ok ill google n see
 
 
-        let a = ''
+        let a = text
         switch (textColour.toLowerCase()) {
             case 'dark':
             case 'black':
             case 'preto':
             case 'negro':
-                a = a + "\x1b[30m";
+                a =  "\x1b[30m" + a + reset;
                 break;
                 
             case 'red':
             case 'vermelho':
-                a = a + "\x1b[31m"
+                a = "\x1b[31m"  + a + reset;
                 break;
 
             case 'green':
             case 'verde':
-                a = a + "\x1b[32m"
+                a =  "\x1b[32m" + a + reset;
                 break;
 
             case 'yellow':
             case 'amarelo':
-                a = a + "\x1b[33m"
+                a = "\x1b[33m" + a + reset;
                 break;
                 
             case 'blue':
             case 'azul':
-                a = a + "\x1b[34m"
+                a = "\x1b[34m" + a + reset;
                 break;
                 
             case 'magenta': 
-            a = a + "\x1b[35m"
+            a = "\x1b[35m"  + a + reset;
                 break;
 
             case 'cyan':
             case 'ciano':
-                a = a + "\x1b[36m"
+                a = "\x1b[36m" + a + reset;
                 break;
             
             case 'light':
-            case 'branco': // white
-            case 'claro': // light
+            case 'branco':
+            case 'claro': 
             case 'white':
-                a = a + "\x1b[37m"
+                a = "\x1b[37m"  + a + reset;
                 break;
 
 
@@ -73,12 +72,12 @@ const colour = {
             case 'negro':
             case 'dark':
             case 'preto':
-                a = a + "\x1b[40m" + text + reset
+                a = "\x1b[40m" + a + reset;
                 break;
             
             case 'red':
             case 'vermelho':
-                a = a + "\x1b[41m" + text + reset;
+                a = "\x1b[41m" + a + reset;
                 break;
 
             case 'green':
@@ -88,39 +87,35 @@ const colour = {
             
             case 'yellow':
             case 'amarelo':
-                a = a + "\x1b[43m" + text + reset;
+                a = "\x1b[43m"  + a + reset;
                 break;
             
             case 'blue':
             case 'azul':
-                 a = a +"\x1b[44m" + text + reset;
+                 a = "\x1b[44m" + a + reset;
                 break;
 
             case 'magenta':
-                a = a + "\x1b[45m" + text + reset;
+                a = "\x1b[45m" + a + reset;
                 break;
 
             case 'cyan':
             case 'ciano':
-                a = a + "\x1b[46m" + text + reset;
+                a = "\x1b[46m" + a + reset;
                 break;
 
             case 'white':
             case 'light':
             case 'branco':
             case 'claro':
-                a = a + "\x1b[47m" + text + reset;
+                a = "\x1b[47m" + a + reset;
                 break;
 
                 default: 
                 break;
         }
 
-        if (!a.includes(reset)) return a + text + reset
-        else return a
+       return a
     }
-};
 
- console.log(colour.log('CDColours', {}), colour.log('>>', { textColour: 'cyan'}), colour.log('This is a test', { textColour: 'white'}))
- //console.error("\x1b[36mCDColours \x1b[34m>> \x1b[37mNo text was provided")
-export default colour; 
+export default colour;
