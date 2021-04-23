f96db073d20899cdc9cb020a16b0746f1ab9ae6e
SpeckyYT <Specky.fans@gmail.com>
2021-04-23 14:35:26 +0200

    replaced switch case with colours object and helper functions

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,30 @@
-"use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+
 const reset = '\x1b[0m';
+
+const colours = {
+    // ENGLISH
+    black: 0, dark: 0,
+    red: 1,
+    green: 2,
+    yellow: 3,
+    blue: 4,
+    magenta: 5,
+    cyan: 6,
+    light: 7,
+    white: 7,
+    // PORTUGUESE
+    negro: 0, preto: 0,
+    vermelho: 1,
+    verde: 2,
+    amarelo: 3,
+    azul: 4,
+    magenta: 5,
+    ciano: 6,
+    branco: 7,
+    claro: 7,
+}
+
 /**
      * @param {string} text - The text which you want to put a colour
      * @param {string} textColour - The text colour you want (Object)
@@ -10,90 +34,26 @@ const reset = '\x1b[0m';
      *
      * colour('Hello World', {textColour: 'blue', backgroundColour: 'green'})
      */
-const colour = (text, { textColour = 'none', backgroundColour = 'none' }) => {
-    if (!text) {
-        return console.error("\x1b[36mCDColours \x1b[34m>> \x1b[37mNo text was provided");
-    }
-    if (typeof text != 'string') {
+const colour = (text, { textColour, backgroundColour }) => {
+    if (typeof text != 'string')
         return console.error("\x1b[36mCDColours \x1b[34m>> \x1b[37mText is not a string, received " + typeof text);
+
+    function resolveColour(colour) {
+        if(typeof colour != 'string') return null;
+        return colours[colour.toLowerCase()];
     }
-    if (textColour == 'none' && backgroundColour == 'none') {
-        return console.error("\x1b[36mCDColours \x1b[34m>> \x1b[37mNo colours were specified, try \x1b[1mcolour('Hello World!', { textColour: 'blue', backgroundColour: 'green' }) \x1b[0minstead");
+
+    function buildColour(string, txtC, bgC){
+        const txt = `${typeof txtC == 'number' ? `\x1b[3${txtC}m` : ''}`;
+        const bg = `${typeof bgC == 'number' ? `\x1b[4${bgC}m` : ''}`;
+        return `${txt}${bg}${string}${reset}`;
     }
-    let a = text;
-    switch (textColour.toLowerCase()) {
-        case 'dark':
-        case 'black':
-        case 'preto':
-        case 'negro':
-            a = "\x1b[30m" + a + reset;
-            break;
-        case 'red':
-        case 'vermelho':
-            a = "\x1b[31m" + a + reset;
-            break;
-        case 'green':
-        case 'verde':
-            a = "\x1b[32m" + a + reset;
-            break;
-        case 'yellow':
-        case 'amarelo':
-            a = "\x1b[33m" + a + reset;
-            break;
-        case 'blue':
-        case 'azul':
-            a = "\x1b[34m" + a + reset;
-            break;
-        case 'magenta':
-            a = "\x1b[35m" + a + reset;
-            break;
-        case 'cyan':
-        case 'ciano':
-            a = "\x1b[36m" + a + reset;
-            break;
-        case 'light':
-        case 'branco':
-        case 'claro':
-        case 'white':
-            a = "\x1b[37m" + a + reset;
-    }
-    switch (backgroundColour.toLowerCase()) {
-        case 'black':
-        case 'negro':
-        case 'dark':
-        case 'preto':
-            a = "\x1b[40m" + a + reset;
-            break;
-        case 'red':
-        case 'vermelho':
-            a = "\x1b[41m" + a + reset;
-            break;
-        case 'green':
-        case 'verde':
-            a = "\x1b[42m" + text + reset;
-            break;
-        case 'yellow':
-        case 'amarelo':
-            a = "\x1b[43m" + a + reset;
-            break;
-        case 'blue':
-        case 'azul':
-            a = "\x1b[44m" + a + reset;
-            break;
-        case 'magenta':
-            a = "\x1b[45m" + a + reset;
-            break;
-        case 'cyan':
-        case 'ciano':
-            a = "\x1b[46m" + a + reset;
-            break;
-        case 'white':
-        case 'light':
-        case 'branco':
-        case 'claro':
-            a = "\x1b[47m" + a + reset;
-    }
-    return a;
+    
+    return buildColour(
+        text,
+        resolveColour(textColour),
+        resolveColour(backgroundColour),
+    );
 };
 module.exports = colour;
 module.exports.default = colour;
